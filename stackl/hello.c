b20a434ec0e4e64839a16f8729974f5dd9601827
alamgirharman <alamgirharman@gmail.com>
2020-05-18 04:35:29 +0600

    learning cpro

--- stackl/hello.c
+++ stackl/hello.c
@@ -4,6 +4,13 @@ int main() {
     printf("Hello World\n");
     printf("Again Hello World\n");
     printf("Print third line\n");
+    printf("Print fourth line\n");
+
+
+    printf("%d\n", 5 + 10);
+    printf("%d\n", 50 - 10);
+    printf("%d\n", 5 * 10);
+    printf("%d\n", 50 / 5);
     return 0;
 }
 
