6296801599e4c3a06a0a3c0e89e6a7e8d160e217
alamgirharman <alamgirharman@gmail.com>
2020-06-13 06:52:58 +0600

    some change

--- cpbsubeen/hello.c
+++ cpbsubeen/hello.c
@@ -5,6 +5,9 @@ int main()
     printf("Hello World\n");
     printf("Hello C Programming\n");
     
-    
+    int a = 6;
+    int b = 5;
+    int sum = a + b;
+    printf("%d + %d = %d", a, b, sum);
     return 0;
 }
