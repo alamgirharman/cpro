3f1dc6b9ffa63602e76d20dda50b84483df8cd79
alamgirharman <alamgirharman@gmail.com>
2020-06-20 10:29:24 +0600

    completed chapter 2

---
+++ cpbsubeen/two16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Test program - comment 1
+    printf("Hello ");
+    /* We have printed hello,
+    now we shall print World.
+     */
+
+    printf("World"); // printed world
+    return 0;
+}
