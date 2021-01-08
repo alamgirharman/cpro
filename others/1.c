b3e12cb0b9c3cf0d5d9d9f87c7c73161f8a9c36d
alamgirharman <alamgirharman@gmail.com>
2021-01-08 22:43:48 +0600

    others modified

--- others/1.c
+++ others/1.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main () {
-    double a, b, sum;
-    scanf("%lf", &a);
-    scanf("%lf", &b);
-    sum = a + b;
-    printf("Sum is: %0.2lf\n", sum);
+    
+    int Arman = 5;
+    int arman = 4;
+
+    int summ = Arman + arman;
+
+    printf("%d", summ);
 
     return 0;
 }
